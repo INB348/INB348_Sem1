751e4e77c79614f7ae82c04ff9f95e816299cfaf
Kristian Matzen <kristian_motzen@hotmail.com>
2014-09-17 11:45:14 +1000

    Adding a new group works with parse

---
+++ INB348/NewGroupViewController.h
@@ -0,0 +1,16 @@
+//
+//  NewGroupViewController.h
+//  INB348
+//
+//  Created by Kristian M Matzen on 17/09/14.
+//  Copyright (c) 2014 nOrJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+#import "GroupsTableViewController.h"
+
+@interface NewGroupViewController : UITableViewController
+@property (strong, nonatomic) IBOutlet UITextField *nameTextField;
+- (IBAction)save:(id)sender;
+@end
